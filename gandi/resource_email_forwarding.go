8a655bf1ea0afb81f34474904676dc14f9830e44
Marius Karnauskas <1094012+karnauskas@users.noreply.github.com>
2020-10-09 11:54:24 +0100

    Add resources for mailbox and forwarding

---
+++ gandi/resource_email_forwarding.go
@@ -0,0 +1,148 @@
+package gandi
+
+import (
+	"github.com/go-gandi/go-gandi/email"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"sort"
+	"strings"
+)
+
+func resourceEmailForwarding() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"domain": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Domain name",
+			},
+			"source": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Account alias name",
+			},
+			"destination": {
+				Type:        schema.TypeList,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Required:    true,
+				Description: "Forwards to email address",
+			},
+		},
+		Create: resourceEmailForwardingCreate,
+		Delete: resourceEmailForwardingDelete,
+		Read:   resourceEmailForwardingRead,
+		Update: resourceEmailForwardingUpdate,
+		Importer: &schema.ResourceImporter{
+			State: resourceEmailForwardingImport,
+		},
+	}
+}
+
+func splitID(id string) (source, domain string) {
+	parts := strings.SplitN(id, "@", 2)
+	return parts[0], parts[1]
+}
+
+func resourceEmailForwardingCreate(d *schema.ResourceData, meta interface{}) (err error) {
+	client := meta.(*clients).Email
+	domain := d.Get("domain").(string)
+	source := d.Get("source").(string)
+
+	var destinations []string
+	for _, i := range d.Get("destination").([]interface{}) {
+		destinations = append(destinations, i.(string))
+	}
+	sort.Strings(destinations)
+
+	request := email.CreateForwardRequest{
+		Source:       source,
+		Destinations: destinations,
+	}
+
+	err = client.CreateForward(domain, request)
+	if err != nil {
+		return
+	}
+
+	d.SetId(source + "@" + domain)
+
+	return resourceEmailForwardingRead(d, meta)
+}
+
+func resourceEmailForwardingRead(d *schema.ResourceData, meta interface{}) (err error) {
+	client := meta.(*clients).Email
+	source, domain := splitID(d.Id())
+
+	forwards, err := client.GetForwards(domain)
+	if err != nil {
+		return
+	}
+
+	var response email.GetForwardRequest
+
+	for _, found := range forwards {
+		if found.Source == source {
+			response = found
+			break
+		}
+	}
+
+	d.Set("href", response.Href)
+	//sort.Strings(response.Destinations)
+	d.Set("destination", response.Destinations)
+	return
+}
+
+func resourceEmailForwardingUpdate(d *schema.ResourceData, meta interface{}) (err error) {
+	client := meta.(*clients).Email
+	source, domain := splitID(d.Id())
+
+	var destinations []string
+	for _, i := range d.Get("destination").([]interface{}) {
+		destinations = append(destinations, i.(string))
+	}
+	//sort.Strings(destinations)
+
+	request := email.UpdateForwardRequest{
+		Destinations: destinations,
+	}
+
+	err = client.UpdateForward(domain, source, request)
+	if err != nil {
+		return
+	}
+	return resourceEmailForwardingRead(d, meta)
+}
+
+func resourceEmailForwardingDelete(d *schema.ResourceData, meta interface{}) (err error) {
+	client := meta.(*clients).Email
+	source, domain := splitID(d.Id())
+
+	err = client.DeleteForward(domain, source)
+	return
+}
+
+func resourceEmailForwardingImport(d *schema.ResourceData, meta interface{}) (data []*schema.ResourceData, err error) {
+	client := meta.(*clients).Email
+	source, domain := splitID(d.Id())
+
+	forwards, err := client.GetForwards(domain)
+	if err != nil {
+		return
+	}
+
+	var response email.GetForwardRequest
+
+	for _, found := range forwards {
+		if found.Source == source {
+			response = found
+			break
+		}
+	}
+
+	d.Set("href", response.Href)
+	sort.Strings(response.Destinations)
+	d.Set("destination", response.Destinations)
+
+	data = []*schema.ResourceData{d}
+	return
+}
---
+++ gandi/resource_mailbox.go
@@ -0,0 +1,138 @@
+package gandi
+
+import (
+	"github.com/go-gandi/go-gandi/email"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"sort"
+	"time"
+)
+
+func resourceMailbox() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"domain": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Domain name",
+			},
+			"login": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Login",
+			},
+			"password": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Sensitive:   true,
+				Description: "Password",
+			},
+			"mailbox_type": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "standard",
+				Description: "Mailbox type",
+			},
+			"aliases": {
+				Type:        schema.TypeList,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Optional:    true,
+				Description: "Aliases for email",
+			},
+		},
+		Create: resourceMailboxCreate,
+		Delete: resourceMailboxDelete,
+		Read:   resourceMailboxRead,
+		Update: resourceMailboxUpdate,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+	}
+}
+
+func resourceMailboxCreate(d *schema.ResourceData, meta interface{}) (err error) {
+	client := meta.(*clients).Email
+	domain := d.Get("domain").(string)
+	login := d.Get("login").(string)
+
+	var aliases []string
+	for _, i := range d.Get("aliases").([]interface{}) {
+		aliases = append(aliases, i.(string))
+	}
+	sort.Strings(aliases)
+
+	request := email.CreateEmailRequest{
+		Aliases:     aliases,
+		Login:       login,
+		MailboxType: d.Get("mailbox_type").(string),
+		Password:    d.Get("password").(string),
+	}
+
+	err = client.CreateEmail(domain, request)
+	if err != nil {
+		return
+	}
+
+	// Sent, got 202 response. What's now?
+	time.Sleep(2 * time.Second)
+
+	boxes, err := client.ListMailboxes(domain)
+	if err != nil {
+		return
+	}
+
+	for _, b := range boxes {
+		if b.Login == login {
+			d.SetId(b.ID)
+			break
+		}
+	}
+
+	return resourceMailboxRead(d, meta)
+}
+
+func resourceMailboxRead(d *schema.ResourceData, meta interface{}) (err error) {
+	client := meta.(*clients).Email
+	domain := d.Get("domain").(string)
+
+	found, err := client.GetMailbox(domain, d.Id())
+	if err != nil {
+		return
+	}
+
+	d.Set("address", found.Address)
+	d.Set("aliases", found.Aliases)
+	d.Set("domain", found.Domain)
+	d.Set("href", found.Href)
+	d.Set("quota_used", found.QuotaUsed)
+	d.Set("login", found.Login)
+	d.Set("mailbox_type", found.MailboxType)
+	return
+}
+
+func resourceMailboxUpdate(d *schema.ResourceData, meta interface{}) (err error) {
+	client := meta.(*clients).Email
+	domain := d.Get("domain").(string)
+
+	var aliases []string
+	for _, i := range d.Get("aliases").([]interface{}) {
+		aliases = append(aliases, i.(string))
+	}
+	sort.Strings(aliases)
+
+	request := email.UpdateEmailRequest{
+		Aliases:  aliases,
+		Login:    d.Get("login").(string),
+		Password: d.Get("password").(string),
+	}
+
+	err = client.UpdateEmail(domain, d.Id(), request)
+	return
+}
+
+func resourceMailboxDelete(d *schema.ResourceData, meta interface{}) (err error) {
+	client := meta.(*clients).Email
+	domain := d.Get("domain").(string)
+
+	err = client.DeleteEmail(domain, d.Id())
+	return
+}
