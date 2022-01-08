5f783bcbb7634bcab4c8b511d1440d4cc613172c
Vincent Bernat <vincent@bernat.ch>
2022-01-08 16:37:33 +0100

    livedns: add a resource to manipulate domain keys
    There are one important shortcoming with this implementation:
    
     - When creating a key, the API does not return its UUID. We guess the
       UUID to be the last one, but this is quite a wild guest. Gandi
       should provide the UUID in the return of the API call.
    
    I didn't generate the documentation as I expect this to be done
    prior to releasing.

--- gandi/provider.go
+++ gandi/provider.go
@@ -51,6 +51,7 @@ func Provider() *schema.Provider {
 		ResourcesMap: map[string]*schema.Resource{
 			"gandi_livedns_domain":         resourceLiveDNSDomain(),
 			"gandi_livedns_record":         resourceLiveDNSRecord(),
+			"gandi_livedns_key":            resourceLiveDNSKey(),
 			"gandi_domain":                 resourceDomain(),
 			"gandi_mailbox":                resourceMailbox(),
 			"gandi_email_forwarding":       resourceEmailForwarding(),
