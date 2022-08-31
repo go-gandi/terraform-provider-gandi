55d99ae8752ae66ebe9cebb4d90975b7a87e38bb
David Flanagan <david@rawkode.dev>
2022-08-31 22:22:31 +0100

    fix: module name
    Co-authored-by: lewo <lewo@abesis.fr>

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/go-gandi/terraform-provider-gandi/gandi/v2"
+	"github.com/go-gandi/terraform-provider-gandi/v2/gandi"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
